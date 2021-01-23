91bb4bc0a4bd97b1886b190ac125e80fc8b5ac36
pruthvi-naskanti <pruthvinaskanti@gmail.com>
2021-01-22 22:47:12 -0600

    commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { QuestModule } from './quest/quest.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
